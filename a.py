6d419b4eae421b575d2fb43e3df4deb9062a5a98
Aryan singh <100441867+aryan121111@users.noreply.github.com>
2023-10-30 02:40:41 +0530

    yes
    yes

---
+++ a.py
@@ -0,0 +1,21 @@
+from flask import Flask, jsonify, render_template
+import openai
+import os
+openai.api_key = os.environ.get('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')
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+  return render_template('a.html')
+  
+@app.route('/generateimages/<prompt>')
+def generate(prompt):
+  print("prompt:", prompt)
+  response = openai.Image.create(prompt=prompt, n=5, size="256x256") 
+  print(response)
+  return jsonify(response)
+
+
+app.run(debug=True)
